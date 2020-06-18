c2f07d66a294b07062fa13bd1764adab95a77de8
mauricechouam <lansichouamou@gmail.com>
2020-06-17 21:56:05 -0700

    updated source code html_route

--- develop/routes/html_route.js
+++ develop/routes/html_route.js
@@ -2,25 +2,26 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var path = require("path");
+const express = require("express")
+const router = express.Router();
+const path =require("path");
 
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("*", function(req, res) {
+  router.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  app.get("/notes", function(req, res) {
+  router.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
-};
+  module.exports = router
